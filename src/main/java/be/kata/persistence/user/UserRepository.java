78de67cf4b3aaf431f1886acb4afa54132f9418a
Godwin Ruswelt <godwin.ruswelt@bosa.fgov.be>
2025-02-08 13:40:03 +0100

    Added user components

---
+++ src/main/java/be/kata/persistence/user/UserRepository.java
@@ -0,0 +1,7 @@
+package be.kata.persistence.user;
+
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface UserRepository {
+}
---
+++ src/main/java/be/kata/service/UserService.java
@@ -0,0 +1,8 @@
+package be.kata.service;
+
+import org.springframework.stereotype.Service;
+
+@Service
+public class UserService {
+
+}

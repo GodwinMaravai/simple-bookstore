3562f96d56ed48b53c690cc43a5310bbabdb5705
Godwin Ruswelt <godwin.ruswelt@bosa.fgov.be>
2025-02-09 00:32:31 +0100

    Adapted Cart with dependencies

--- src/main/java/be/kata/api/CartController.java
+++ src/main/java/be/kata/api/CartController.java
@@ -1,7 +1,14 @@
 package be.kata.api;
 
+import be.kata.service.CartService;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class CartController {
+
+    private final CartService cartService;
+
+    CartController(CartService cartService) {
+        this.cartService = cartService;
+    }
 }
--- src/main/java/be/kata/service/CartService.java
+++ src/main/java/be/kata/service/CartService.java
@@ -1,8 +1,14 @@
 package be.kata.service;
 
+import be.kata.persistence.cart.CartRepository;
 import org.springframework.stereotype.Service;
 
 @Service
 public class CartService {
 
+    private final CartRepository cartRepository;
+
+    CartService(CartRepository cartRepository) {
+        this.cartRepository = cartRepository;
+    }
 }

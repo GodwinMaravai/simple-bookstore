88316c61da7b8a599cf5aae33e0aa10dec493861
Godwin Ruswelt <godwin.ruswelt@bosa.fgov.be>
2025-02-08 10:54:28 +0100

    DataJpaTest - Book repository with necessary curd operation

---
+++ src/test/java/be/kata/persistence/book/BookRepositoryTest.java
@@ -0,0 +1,28 @@
+package be.kata.persistence.book;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
+import org.springframework.test.util.ReflectionTestUtils;
+
+import static org.assertj.core.api.Assertions.assertThatNoException;
+
+@DataJpaTest(properties = {
+        "spring.datasource.url=jdbc:h2:mem:testdb",
+        "spring.jpa.hibernate.ddl-auto=create-drop"
+})
+class BookRepositoryTest {
+
+    @Autowired
+    private BookRepository bookRepository;
+
+    @Test
+    void givenBookRepository_whenInvokeMethod_thenReturn_findAllMethod() {
+        assertThatNoException().isThrownBy(() -> ReflectionTestUtils.invokeMethod(bookRepository, "findAll"));
+    }
+
+    @Test
+    void givenBookRepository_whenInvokeMethod_thenReturn_findByIdMethod() {
+        assertThatNoException().isThrownBy(() -> ReflectionTestUtils.invokeMethod(bookRepository, "findById"));
+    }
+}

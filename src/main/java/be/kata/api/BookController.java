3f000b9ad4565a1c0b3527e44c073fe4e20d60de
Godwin Ruswelt <godwin.ruswelt@bosa.fgov.be>
2025-02-08 12:56:39 +0100

    Added Book Controller Api methods with expected results and status code

--- src/main/java/be/kata/api/BookController.java
+++ src/main/java/be/kata/api/BookController.java
@@ -1,7 +1,45 @@
 package be.kata.api;
 
-import org.springframework.stereotype.Controller;
+import be.kata.api.model.Book;
+import be.kata.service.BookService;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
-@Controller
+import java.util.List;
+
+import static org.springframework.util.ObjectUtils.isEmpty;
+import static org.springframework.util.StringUtils.hasText;
+
+@RestController
+@RequestMapping("/books")
 public class BookController {
+
+    private final BookService bookService;
+
+    BookController(BookService bookService) {
+        this.bookService = bookService;
+    }
+
+    @GetMapping
+    public ResponseEntity<List<Book>> get() {
+        List<Book> books = bookService.getAllBooks();
+        if (isEmpty(books)) {
+            return ResponseEntity.noContent().build();
+        }
+        return ResponseEntity.ok(books);
+    }
+
+    @GetMapping("/{id}")
+    public ResponseEntity<Book> getById(@PathVariable String id) {
+        if (!hasText(id)) {
+            return ResponseEntity.badRequest().build();
+        }
+        return bookService.getBookById(id)
+                .map(ResponseEntity::ok)
+                .orElse(ResponseEntity.noContent().build());
+    }
+
 }

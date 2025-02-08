82406e19eb2eeac2294377df4104e127ccc9a8fc
Godwin Ruswelt <godwin.ruswelt@bosa.fgov.be>
2025-02-08 13:45:03 +0100

    Test - user components dependencies test

--- src/test/java/be/kata/BookStoreAppTest.java
+++ src/test/java/be/kata/BookStoreAppTest.java
@@ -1,7 +1,9 @@
 package be.kata;
 
 import be.kata.api.BookController;
+import be.kata.api.UserController;
 import be.kata.service.BookService;
+import be.kata.service.UserService;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
@@ -9,6 +11,7 @@ import org.springframework.context.ApplicationContext;
 import org.springframework.test.util.ReflectionTestUtils;
 
 import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.Assertions.assertThatNoException;
 
 @SpringBootTest
 class BookStoreAppTest {
@@ -17,21 +20,27 @@ class BookStoreAppTest {
     private ApplicationContext context;
 
     @Test
-    void givenApplicationContext_whenGetBean_thenNotNull() {
+    void givenApplicationContext_whenGetBean_thenNotNullAndDependenciesNotNull() {
         assertThat(context.getBean("bookStoreApp")).isNotNull();
 
-        assertThat(context.getBean("bookController")).isNotNull();
-        assertThat(context.getBean("bookService")).isNotNull();
         assertThat(context.getBean("bookRepository")).isNotNull();
+        assertThat(context.getBean("bookService")).isNotNull();
+        assertThat(context.getBean("bookController")).isNotNull();
 
         BookService bookService = (BookService) context.getBean("bookService");
-        ReflectionTestUtils.getField(bookService, "bookRepository");
+        assertThatNoException().isThrownBy(() -> ReflectionTestUtils.getField(bookService, "bookRepository"));
 
         BookController bookController = (BookController) context.getBean("bookController");
-        ReflectionTestUtils.getField(bookController, "bookService");
+        assertThatNoException().isThrownBy(() -> ReflectionTestUtils.getField(bookController, "bookService"));
 
         assertThat(context.getBean("userRepository")).isNotNull();
         assertThat(context.getBean("userService")).isNotNull();
         assertThat(context.getBean("userController")).isNotNull();
+
+        UserService userService = (UserService) context.getBean("userService");
+        assertThatNoException().isThrownBy(() -> ReflectionTestUtils.getField(userService, "userRepository"));
+
+        UserController userController = (UserController) context.getBean("userController");
+        assertThatNoException().isThrownBy(() -> ReflectionTestUtils.getField(userController, "userService"));
     }
 }

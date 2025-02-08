46671d3c4ff9a31efdb2fe57fe543f69f18af0b7
Godwin Ruswelt <godwin.ruswelt@bosa.fgov.be>
2025-02-08 13:04:12 +0100

    Refactored - Add Spring Boot app and book components test

--- src/test/java/be/kata/BookStoreAppTest.java
+++ src/test/java/be/kata/BookStoreAppTest.java
@@ -1,9 +1,12 @@
 package be.kata;
 
+import be.kata.api.BookController;
+import be.kata.service.BookService;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.context.ApplicationContext;
+import org.springframework.test.util.ReflectionTestUtils;
 
 import static org.assertj.core.api.Assertions.assertThat;
 
@@ -16,8 +19,15 @@ class BookStoreAppTest {
     @Test
     void givenApplicationContext_whenGetBean_thenNotNull() {
         assertThat(context.getBean("bookStoreApp")).isNotNull();
+
         assertThat(context.getBean("bookController")).isNotNull();
         assertThat(context.getBean("bookService")).isNotNull();
         assertThat(context.getBean("bookRepository")).isNotNull();
+
+        BookService bookService = (BookService) context.getBean("bookService");
+        ReflectionTestUtils.getField(bookService, "bookRepository");
+
+        BookController bookController = (BookController) context.getBean("bookController");
+        ReflectionTestUtils.getField(bookController, "bookService");
     }
 }

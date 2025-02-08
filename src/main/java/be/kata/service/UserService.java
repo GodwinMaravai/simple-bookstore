d3ece8622f2852dee8df8b7acf4d3a42fc9dc6d7
Godwin Ruswelt <godwin.ruswelt@bosa.fgov.be>
2025-02-08 14:57:09 +0100

    Added User service with necessary business operation

--- src/main/java/be/kata/service/UserService.java
+++ src/main/java/be/kata/service/UserService.java
@@ -1,7 +1,10 @@
 package be.kata.service;
 
+import be.kata.api.model.User;
+import be.kata.persistence.user.UserEntity;
 import be.kata.persistence.user.UserRepository;
 import org.springframework.stereotype.Service;
+import org.springframework.util.Assert;
 
 @Service
 public class UserService {
@@ -10,5 +13,19 @@ public class UserService {
 
     UserService(UserRepository userRepository) {
         this.userRepository = userRepository;
+    }
+
+    public boolean isUserExist(long id) {
+        return userRepository.findById(id).isPresent();
+    }
+
+    public boolean createUser(User user) {
+        Assert.notNull(user, "Invalid user");
+        UserEntity userEntity = new UserEntity();
+        userEntity.setNrn(user.nrn());
+        userEntity.setName(user.name());
+        userEntity.setGsm(user.gsm());
+        userRepository.save(userEntity);
+        return true;
     }
 }

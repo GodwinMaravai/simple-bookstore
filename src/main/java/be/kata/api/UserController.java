eb1c79f511095ff8fab1b6a40422c6e092df568b
Godwin Ruswelt <godwin.ruswelt@bosa.fgov.be>
2025-02-09 20:32:45 +0100

    Adapted User login api

--- src/main/java/be/kata/api/UserController.java
+++ src/main/java/be/kata/api/UserController.java
@@ -24,7 +24,7 @@ public class UserController {
         this.userService = userService;
     }
 
-    @PostMapping("/login")
+    @GetMapping("/login")
     public ResponseEntity<String> login(Authentication authentication) {
         return ResponseEntity.ok("Welcome '%s'".formatted(authentication.getName()));
     }

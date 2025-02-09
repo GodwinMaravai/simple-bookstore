8799438ec413ef01573bf8d85d027b8ae32c135e
Godwin Ruswelt <godwin.ruswelt@bosa.fgov.be>
2025-02-09 13:40:41 +0100

    Test - User repository refactored

--- src/test/java/be/kata/persistence/user/UserRepositoryTest.java
+++ src/test/java/be/kata/persistence/user/UserRepositoryTest.java
@@ -1,10 +1,14 @@
 package be.kata.persistence.user;
 
+import be.kata.persistence.cart.CartEntity;
+import be.kata.persistence.cart.CartItemEntity;
 import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
+
+import java.util.Set;
 
 import static org.assertj.core.api.Assertions.assertThat;
 
@@ -15,6 +19,7 @@ import static org.assertj.core.api.Assertions.assertThat;
 class UserRepositoryTest {
 
     private UserEntity userEntity;
+    private CartEntity cartEntity;
 
     @Autowired
     private UserRepository userRepository;
@@ -25,6 +30,19 @@ class UserRepositoryTest {
         userEntity = new UserEntity();
         userEntity.setName("User2");
         userEntity.setNrn("12345678902");
+
+        CartItemEntity cartItemEntity1 = new CartItemEntity();
+        cartItemEntity1.setBookId("B1");
+        cartItemEntity1.setCount(1);
+
+        CartItemEntity cartItemEntity2 = new CartItemEntity();
+        cartItemEntity2.setBookId("B2");
+        cartItemEntity2.setCount(2);
+
+        cartEntity = new CartEntity();
+        cartEntity.setItems(Set.of(cartItemEntity1, cartItemEntity2));
+        userEntity.setCart(cartEntity);
+
         userRepository.save(userEntity);
     }
 
@@ -49,6 +67,7 @@ class UserRepositoryTest {
 
     @Test
     void givenUserRepository_whenSave_thenReturnUserEntity() {
+        userEntity = new UserEntity();
         userEntity.setName("USER");
         userEntity.setNrn("12345678901");
 
@@ -58,4 +77,20 @@ class UserRepositoryTest {
 
         assertThat(userRepository.findById(userEntity.getId())).isNotNull();
     }
+
+    @Test
+    void givenUserWithCart_whenSave_thenReturnUserEntity() {
+        userEntity = new UserEntity();
+        userEntity.setName("USER");
+        userEntity.setNrn("12345678901");
+        userEntity.setCart(cartEntity);
+
+        assertThat(userRepository.save(userEntity))
+                .isNotNull()
+                .isEqualTo(userEntity);
+
+        assertThat(userRepository.findById(userEntity.getId())).isNotNull();
+        assertThat(userRepository.findById(userEntity.getId()).get().getCart()).isEqualTo(cartEntity);
+    }
+
 }
